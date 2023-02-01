5f8c893466fdef633c2a361c9ec838495fbec8a6
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-01 23:32:42 +0700

    Add rules channel

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,14 +1,26 @@
 import { file } from 'bun'
-const response = await fetch(
-  `${process.env.RESOURCES_WEBHOOK_URL}/messages/${process.env.RESOURCES_MESSAGE_ID}`,
-  {
+
+async function sync(filename: string, webhookUrl: string, messageId: string) {
+  const response = await fetch(`${webhookUrl}/messages/${messageId}`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      content: await file('messages/resources.txt').text(),
+      content: await file(filename).text(),
     }),
-  },
+  })
+  console.log('Update', filename, response.status)
+}
+
+await sync(
+  'messages/rules.txt',
+  process.env.RULES_WEBHOOK_URL!,
+  process.env.RULES_MESSAGE_ID!,
 )
-console.log('Update message', response.status)
+
+await sync(
+  'messages/resources.txt',
+  process.env.RESOURCES_WEBHOOK_URL!,
+  process.env.RESOURCES_MESSAGE_ID!,
+)
