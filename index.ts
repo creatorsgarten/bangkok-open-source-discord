2ca3d5574976941fd803f08eeff33597f4ab1f25
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-01 22:45:45 +0700

    Add script to update message

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+const response = await fetch(
+  `${process.env.RESOURCES_WEBHOOK_URL}/messages/${process.env.RESOURCES_MESSAGE_ID}`,
+  {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: 'Hello, world!',
+    }),
+  },
+)
+console.log('Update message', response.status)
