8e6e75f228b67077212f2bc2c6ad206f4ec99f53
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-09 12:23:43 +0700

    Fix unicode problem by breaking into multiple messages

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,26 +1,34 @@
 import { file } from 'bun'
 
-async function sync(filename: string, webhookUrl: string, messageId: string) {
-  const response = await fetch(`${webhookUrl}/messages/${messageId}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      content: await file(filename).text(),
-    }),
-  })
-  console.log('Update', filename, response.status)
+async function sync(
+  filename: string,
+  webhookUrl: string,
+  messageIds: string[],
+) {
+  const contents = (await file(filename).text())
+    .split('\\pagebreak')
+    .map((x) => x.trim())
+  if (contents.length > messageIds.length) {
+    contents.push(contents.splice(messageIds.length - 1).join('\n\n'))
+  }
+  for (const [index, content] of contents.entries()) {
+    const messageId = messageIds[index]
+    const response = await fetch(`${webhookUrl}/messages/${messageId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content }),
+    })
+    console.log('Update', filename, response.status)
+  }
 }
 
-await sync(
-  'messages/rules.txt',
-  process.env.RULES_WEBHOOK_URL!,
-  process.env.RULES_MESSAGE_ID!,
-)
+await sync('messages/rules.txt', process.env.RULES_WEBHOOK_URL!, [
+  '1070380691969290372',
+  '1073110053944565790',
+])
 
-await sync(
-  'messages/resources.txt',
-  process.env.RESOURCES_WEBHOOK_URL!,
-  process.env.RESOURCES_MESSAGE_ID!,
-)
+await sync('messages/resources.txt', process.env.RESOURCES_WEBHOOK_URL!, [
+  '1070367687647166474',
+])
