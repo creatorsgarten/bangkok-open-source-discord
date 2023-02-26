8f494427a02a0da683b78da8930b66a95a44513e
Thai Pangsakulyanont <dtinth@spacet.me>
2023-02-26 10:31:06 +0000

    add id

--- projects.rb
+++ projects.rb
@@ -1,33 +1,33 @@
-Team = Struct.new(:slug, :name, keyword_init: true)
+Team = Struct.new(:id, :slug, :name, keyword_init: true)
 
 teams = [
-  Team.new(slug: 'ratchagitja', name: 'Ratchagitja'),
-  Team.new(slug: 'greenspaces', name: 'Green Spaces'),
-  Team.new(slug: 'bankforthepoor', name: 'Bank for the Poor'),
-  Team.new(slug: 'fillyouintheblank', name: 'Fill You In The Blank'),
-  Team.new(slug: 'smarttrafficlights', name: 'Smart Traffic Lights'),
-  Team.new(slug: 'bkkchangelog', name: 'bkkchangelog'),
+  Team.new(id: 'ratchagitja',        slug: 'ratchagitja',        name: 'Ratchagitja'),
+  Team.new(id: 'greenspaces',        slug: 'greenspaces',        name: 'Green Spaces'),
+  Team.new(id: 'bankforthepoor',     slug: 'bankforthepoor',     name: 'Bank for the Poor'),
+  Team.new(id: 'fillyouintheblank',  slug: 'fillyouintheblank',  name: 'Fill You In The Blank'),
+  Team.new(id: 'smarttrafficlights', slug: 'smarttrafficlights', name: 'Smart Traffic Lights'),
+  Team.new(id: 'bkkchangelog',       slug: 'bkkchangelog',       name: 'bkkchangelog'),
 ]
 
 File.open 'projects.tf', 'w' do |f|
   teams.each do |team|
     f.puts <<~EOF
-      resource "discord_category_channel" "#{team.slug}_category" {
+      resource "discord_category_channel" "#{team.id}_category" {
         name      = "#{team.name}"
         server_id = var.server_id
       }
-      resource "discord_text_channel" "#{team.slug}_text" {
-        name      = "#{team.slug}"
+      resource "discord_text_channel" "#{team.id}_text" {
+        name      = "#{team.id}"
         server_id = var.server_id
-        category  = discord_category_channel.#{team.slug}_category.id
+        category  = discord_category_channel.#{team.id}_category.id
       }
-      resource "discord_role" "#{team.slug}_role" {
-        name        = "proj-#{team.slug}"
+      resource "discord_role" "#{team.id}_role" {
+        name        = "proj-#{team.id}"
         server_id   = var.server_id
         mentionable = true
       }
-      resource discord_message "#{team.slug}_rolemessage" {
-        channel_id = discord_text_channel.#{team.slug}_text.id
+      resource discord_message "#{team.id}_rolemessage" {
+        channel_id = discord_text_channel.#{team.id}_text.id
         content    = "Welcome to the **#{team.name}** project! (React to this message to join.)"
       }
     EOF
