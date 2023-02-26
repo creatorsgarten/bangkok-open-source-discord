bfbfa94427bf535c93abab05828145065da84071
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-26 15:06:55 +0700

    Add tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    discord = {
+      source  = "aequasi/discord"
+      version = "0.0.4"
+    }
+  }
+}
+
+provider "discord" {
+  # Configuration options
+}
